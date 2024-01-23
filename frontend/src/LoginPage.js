3202c11c84472b87ec5020491cc9dd50cb3ae551
Ayobami Ayodeji <ayobaami@hotmail.com>
2024-01-23 10:27:54 +0000

    update reg and login page pic

--- frontend/src/LoginPage.js
+++ frontend/src/LoginPage.js
@@ -25,7 +25,7 @@ const LoginPage = ({ onLogin, loggedIn, wrongEmail }) => {
   return (
     <div className="login-page">
        <h1>Login</h1> {/* Add the H1 element */}
-      <img src="https://images.unsplash.com/photo-1538587888044-79f13ddd7e49?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Login" className="login-image" />
+      <img src="https://simpleeconsa.blob.core.windows.net/simpleeshop/eshop_homepage.png" alt="Login" className="login-image" />
       <div className="login-form">
         <form onSubmit={handleSubmit}>
           <label>
