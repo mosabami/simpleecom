d2eacbf88625629977190c2c834d8206187a7580
Adonis Latham <adonislatham@microsoft.com>
2024-04-30 15:23:54 -0500

    Fix Options errors

--- backend/Simpleecom.Shared/Options/RepositoryOptions.cs
+++ backend/Simpleecom.Shared/Options/RepositoryOptions.cs
@@ -4,7 +4,6 @@
     {
         public string ConnectionString { get; set; }
         public string DatabaseId { get; set; }
-        public string DatabaseName { get; set; }
         public string PartitionKey { get; set; }
         public string ContainerName { get; set; }
         public bool IsAutoResourceCreationIfNotExistsEnabled { get; set; }
